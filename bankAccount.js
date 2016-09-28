f2b1289af95b7885cb39582fde51b68f01796ac9
marufkhan01 <marufkhan@Marufs-MacBook-Pro.local>
2016-09-28 13:46:35 +0200

    bankAccount

---
+++ bankAccount.js
@@ -0,0 +1,18 @@
+function bankAccount() {
+	var pincode = "1234";
+	var bankBalance = "Your balance is €9...";
+	var numberOFAttempts = 0;
+		while (numberOFAttempts < 3) {
+			var userInput = prompt("Please enter your 4 digit pin code");
+			if (userInput === pincode){
+				console.log(bankBalance)
+				return bankBalance;
+				
+			}
+			else
+				numberOFAttempts++;
+
+
+		}console.log('please try it later');
+}
+bankAccount();
